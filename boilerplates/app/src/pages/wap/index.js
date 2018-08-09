77f48c2a1b56a4fb99151283f6d888a5fde48789
fuyun <yun.fu@chinaredstar.com>
2018-08-09 17:11:27 +0800

    提交前端脚手架git 修改

--- boilerplates/app/src/pages/wap/index.js
+++
@@ -1,7 +0,0 @@
-import {webviewSystem} from '../../unit/native';
-import {getUrlParams} from '../../unit/getUrl';
-import './index.scss';
-
-
-console.log(webviewSystem);
-console.log(getUrlParams);
